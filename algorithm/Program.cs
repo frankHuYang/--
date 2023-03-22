e216876ef2ae016a847e6376ff2f86905233218d
frankHuyang <825399147.com>
2023-03-22 18:47:18 +0800

    第5课-创建数组

---
+++ algorithm/Program.cs
@@ -0,0 +1,48 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        var a = new ArrayList(5);
+        a.Add(1);
+        a.Add(2);
+        a.Add(3);
+        a.Add(4);
+        a.Add(5);
+        Console.WriteLine(a);
+        a.Delete(4);
+        Console.WriteLine(a);
+
+        a.Delete(5);
+        Console.WriteLine(a);
+
+        a.Delete(3);
+        Console.WriteLine(a);
+        a.Delete(1);
+        Console.WriteLine(a);
+        a.Delete(2);
+        Console.WriteLine(a);
+        a.Delete(6);
+        Console.WriteLine(a);
+        a.Delete(6);
+        Console.WriteLine(a.FindIndex(1));
+        Console.WriteLine(a.FindIndex(6));
+        a.Add(1);
+        a.Add(2);
+        a.Add(3);
+        a.Add(4);
+        a.Add(5);
+        a.Add(6);
+        a.Add(7);
+        a.Add(8);
+        a.Add(9);
+        a.Add(10);
+        Console.WriteLine(a);
+        Console.WriteLine(a.FindIndex(5));
+        Console.WriteLine(a.FindIndex(10));
+        Console.WriteLine(a.FindIndex(11));
+        a.Insert(4, 5);
+        Console.WriteLine(a);
+    }
+}
+
---
+++ algorithm/array/ArrayList.cs
@@ -0,0 +1,124 @@
+public class ArrayList
+{
+
+    private int[] _array;
+    private const int _defaultCapacity = 10;
+    private int currentCount = 0;
+    public ArrayList() : this(_defaultCapacity)
+    {
+
+    }
+
+    public ArrayList(int capacity)
+    {
+        if (capacity <= 0) throw new ArgumentOutOfRangeException($"{capacity}必须大于0");
+        _array = new int[capacity];
+    }
+
+
+    private bool HaveCapacity()
+    {
+        return currentCount < _array.Length;
+    }
+    private void AddCapacity()
+    {
+        var newArray = new int[_array.Length * 2];
+        for (var i = 0; i < currentCount - 1; i++)
+        {
+            newArray[i] = _array[i];
+        }
+        _array = newArray;
+    }
+
+    public int FindIndex(int value)
+    {
+
+        for (var i = 0; i < currentCount - 1; i++)
+        {
+            if (value == _array[i])
+            {
+
+                return i;
+            }
+        }
+        return -1;  //TODO:这里是返回-1好？还是抛出异常好呢?用户的预期是必须找到，还是可以不找到呢？
+    }
+
+    public void Add(int value)
+    {
+
+        if (HaveCapacity())
+        {
+            _array[currentCount++] = value;
+        }
+        else
+        {
+            AddCapacity();
+            _array[currentCount++] = value;
+        }
+
+    }
+
+    public void Insert(int index, int value)
+    {
+
+        if (HaveCapacity())
+        {
+
+            for (var i = currentCount - 1; i >= index; i--)
+            {
+
+                _array[i + 1] = _array[i];
+            }
+            _array[index] = value;
+            currentCount++;
+
+        }
+        else
+        {
+            AddCapacity();
+            Insert(index, value);
+
+        }
+
+
+    }
+
+    public void Delete(int value)
+    {
+        var index = FindIndex(value);
+        if (index != -1)
+        {
+            RemoveAt(index);
+        }
+
+    }
+
+    public void RemoveAt(int index)
+    {
+
+        if (index < 0)
+        {
+            throw new ArgumentOutOfRangeException("index不能小0");
+        }
+        if (index > currentCount - 1)
+        {
+            throw new ArgumentOutOfRangeException($"index不能大于{currentCount - 1}");
+        }
+
+        for (var i = index; i < currentCount - 1; i++)
+        {
+            _array[i] = i == currentCount - 1 ? 0 : _array[i + 1];
+        }
+        currentCount--;
+        _array[currentCount - 1] = 0;
+
+    }
+
+    public override string ToString()
+    {
+        return string.Join('-', _array.Take(currentCount));
+    }
+
+
+}
